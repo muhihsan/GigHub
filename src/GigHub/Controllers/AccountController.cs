06e8dec65f7c611eaccc1d0607dc4f1fd34de8f1
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-13 23:40:46 +1000

    Remove unnecessary using statement

--- src/GigHub/Controllers/AccountController.cs
+++ src/GigHub/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
@@ -9,7 +7,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using GigHub.Models;
 using GigHub.Models.AccountViewModels;
 using GigHub.Services;
--- src/GigHub/Controllers/ManageController.cs
+++ src/GigHub/Controllers/ManageController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using GigHub.Models;
 using GigHub.Models.ManageViewModels;
 using GigHub.Services;
