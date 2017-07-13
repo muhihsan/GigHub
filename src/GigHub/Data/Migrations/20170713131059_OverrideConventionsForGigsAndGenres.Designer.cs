f17cccfccabf88ae0c5b583c3ddb01678832f080
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-13 23:12:40 +1000

    Add Migrations for adding DataAnnotations to Gigs and Genres

--- src/GigHub/Data/Migrations/20170713131059_OverrideConventionsForGigsAndGenres.Designer.cs
+++ src/GigHub/Data/Migrations/20170713131059_OverrideConventionsForGigsAndGenres.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace GigHub.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170713131059_OverrideConventionsForGigsAndGenres")]
+    partial class OverrideConventionsForGigsAndGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
