0f76c1c0289f7fae31aa4e58a7299ddd02156681
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-13 15:49:33 +1000

    Add new project files that include default asp user authentication

---
+++ src/GigHub/Areas/IdentityService/IdentityServiceConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.Service.Extensions;
+
+namespace GigHub.Identity
+{
+    // These values are used to setup the identity servcie and should not be changed
+    public class IdentityServiceConstants
+    {
+        // Name of the single identity service tenant
+        public const string Tenant = "IdentityService";
+        
+        // Unique ID of the single identity service tenant
+        public const string TenantId = "2EE3391F-9A0E-4FAC-80E5-F096B76F9F1B";
+
+        // Default policy for the identity service
+        public const string DefaultPolicy = "signinsignup";
+
+        // Identity service version
+        public const string Version = "2.0";
+
+        // Identity service token version
+        public const string TokenVersion = "1.0";
+    }
+}
