32c1d4eac0dc572c20939f5ee2ed1a15ea3105a1
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-14 16:51:20 +1000

    Add action to create gig async

--- src/GigHub/Controllers/GigsController.cs
+++ src/GigHub/Controllers/GigsController.cs
@@ -1,18 +1,24 @@
 ﻿using GigHub.Data;
+using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public GigsController(ApplicationDbContext context)
+        public GigsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         
         [Authorize]
@@ -23,6 +29,28 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(GigFormViewModel viewModel)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var artist = _context.Users.Single(u => u.Id == user.Id);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            await _context.Gigs.AddAsync(gig);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
