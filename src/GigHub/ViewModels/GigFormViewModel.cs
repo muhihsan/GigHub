0ce2a7514a7ca9834f31931d56d5efe610a22eb1
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-14 15:47:57 +1000

    Add view model for gig

---
+++ src/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
