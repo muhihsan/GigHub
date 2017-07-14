e0d15d5abd171ff2d1480802fc60cd1fc701b8e6
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-14 15:26:23 +1000

    Add default value to Genres by using Migration

--- src/GigHub/Data/Migrations/20170713131653_PopulateGenresTable.cs
+++ src/GigHub/Data/Migrations/20170713131653_PopulateGenresTable.cs
@@ -6,12 +6,15 @@ namespace GigHub.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Jazz')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Blues')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Rock')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Country')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Genres WHERE Id IN (1, 2, 3, 4)");
         }
     }
 }
