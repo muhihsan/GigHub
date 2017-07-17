80edc015d857eecc434fd9a5e6537daa98bc3f87
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-17 16:59:50 +1000

    Add validation for the Date of GigFormViewModel

---
+++ src/GigHub/ViewModels/FutureDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigHub.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "d MMM yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out var dateTime);
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
