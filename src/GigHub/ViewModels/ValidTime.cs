bccc43305037eb659184df9bb3e6336ec2270687
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-17 22:35:45 +1000

    Add ValidTime ValidationAttribute to check Time format

---
+++ src/GigHub/ViewModels/ValidTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigHub.ViewModels
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "HH:mm", CultureInfo.CurrentCulture, DateTimeStyles.None, out var dateTime);
+            return isValid;
+        }
+    }
+}
