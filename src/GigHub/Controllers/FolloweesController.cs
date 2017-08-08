e0b42cc5eef70ab00a38f8a64e48f78e282eafc8
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-08 23:07:25 +1000

    Add controller action to return list of actor/actress that we're following

---
+++ src/GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Data;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FolloweesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FolloweesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var followings = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(followings);
+        }
+    }
+}
