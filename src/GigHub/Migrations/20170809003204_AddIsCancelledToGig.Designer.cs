4e92ac114ec4dc5742fda35d213aaa3e67c6ecac
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-09 10:33:23 +1000

    Add IsCancelled property to Gig class and add its migration

--- src/GigHub/Migrations/20170809003204_AddIsCancelledToGig.Designer.cs
+++ src/GigHub/Migrations/20170809003204_AddIsCancelledToGig.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace GigHub.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170809003204_AddIsCancelledToGig")]
+    partial class AddIsCancelledToGig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GigHub/Migrations/20170809003204_AddIsCancelledToGig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GigHub.Migrations
+{
+    public partial class AddIsCancelledToGig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCancelled",
+                table: "Gigs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCancelled",
+                table: "Gigs");
+        }
+    }
+}
