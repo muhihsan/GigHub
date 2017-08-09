061bacb5e19a288bbb10be3b1ac8ec3dae3b2d82
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-09 10:41:58 +1000

    Add GigsApiController to cancel a gig

---
+++ src/GigHub/Controllers/Api/GigsApiController.cs
@@ -0,0 +1,39 @@
+﻿using GigHub.Data;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    [Route("api/gigs")]
+    public class GigsApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GigsApiController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpDelete("cancel")]
+        public async Task<IActionResult> Cancel(int id)
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var gig = _context.Gigs
+                .Single(g => g.Id == id && g.ArtistId == userId);
+
+            gig.IsCancelled = true;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
