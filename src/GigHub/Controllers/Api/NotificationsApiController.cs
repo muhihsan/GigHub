d4061e4f0bef3b11994117de5ba485e3b6ddd2f2
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-10 17:16:51 +1000

    Return only those notifications that have not been read

--- src/GigHub/Controllers/Api/NotificationsApiController.cs
+++ src/GigHub/Controllers/Api/NotificationsApiController.cs
@@ -29,7 +29,7 @@ namespace GigHub.Controllers.Api
             var userId = _userManager.GetUserId(User);
 
             var notifications = _context.UserNotifications
-                .Where(un => un.UserId == userId)
+                .Where(un => un.UserId == userId && !un.IsRead)
                 .Select(un => un.Notification)
                 .Include(n => n.Gig.Artist);
 
