d0a4dc533e9af9d40d4bd7adfcd9e22443715e9f
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-10 16:33:36 +1000

    Add notifications api controller which will return list of notifications

---
+++ src/GigHub/Controllers/Api/NotificationsApiController.cs
@@ -0,0 +1,37 @@
+﻿using GigHub.Data;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    [Route("api/notifications")]
+    public class NotificationsApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public NotificationsApiController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet("newNotifications")]
+        public IActionResult GetNewNotifications()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist);
+
+            return Ok(notifications);
+        }
+    }
+}
