7d9dea3cedb163897420aaa0b3294719b966e820
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-12 22:31:54 +1000

    Refactor javascript code to have better separation of concern

---
+++ src/GigHub/wwwroot/js/controllers/gigsController.js
@@ -0,0 +1,28 @@
+﻿var GigsController = function (attendanceService) {
+    var button;
+
+    var init = function (container) {
+        $(container).on('click', '.js-toggle-attendance', toggleAttendance);
+    };
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+        var gigId = button.attr("data-gig-id");
+        button.hasClass('btn-default') ?
+            attendanceService.attend(gigId, success, error) :
+            attendanceService.cancelAttendance(gigId, success, error);
+    };
+
+    var success = function () {
+        var text = button.text() === 'Going' ? 'Going ?' : 'Going';
+        button.toggleClass('btn-info').toggleClass('btn-default').text(text);
+    };
+
+    var error = function () {
+        alert("Something failed!");
+    };
+
+    return {
+        init: init
+    };
+}(AttendanceService);
---
+++ src/GigHub/wwwroot/js/services/attendanceService.js
@@ -0,0 +1,26 @@
+﻿var AttendanceService = function () {
+    var attend = function (gigId, success, error) {
+        $.ajax({
+            url: "/api/attendances/attend",
+            method: "POST",
+            data: JSON.stringify({ "GigId": gigId }),
+            contentType: "application/json; charset=utf-8",
+            success: success,
+            error: error
+        });
+    };
+
+    var cancelAttendance = function (gigId, success, error) {
+        $.ajax({
+            url: "/api/attendances/" + gigId,
+            method: "DELETE",
+            success: success,
+            error: error
+        });
+    };
+
+    return {
+        attend: attend,
+        cancelAttendance: cancelAttendance
+    };
+}();
