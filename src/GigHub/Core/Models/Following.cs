6928afb5714135ca7035dd0536feb503644d9606
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-23 12:07:03 +1000

    Remove data annotation from model

--- src/GigHub/Core/Models/Following.cs
+++ src/GigHub/Core/Models/Following.cs
@@ -1,17 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace GigHub.Core.Models
+﻿namespace GigHub.Core.Models
 {
     public class Following
     {
-        [Column(Order = 1)]
         public string FollowerId { get; set; }
-
-        [Column(Order = 2)]
         public string FolloweeId { get; set; }
-
         public ApplicationUser Follower { get; set; }
-
         public ApplicationUser Followee { get; set; }
     }
 }
