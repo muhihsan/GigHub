010a6c8aab8b6c2851ea6aae32104010d10ccfbb
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-25 15:24:34 +1000

    Add unit test for gig Modify method

--- src/GigHub.Tests/Domain/Models/GigTests.cs
+++ src/GigHub.Tests/Domain/Models/GigTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using GigHub.Core.Models;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -7,6 +8,38 @@ namespace GigHub.Tests.Domain.Models
 {
     public class GigTests
     {
+        [Fact]
+        public void Modify_WhenCalled_ShouldSetVenueDateTimeGenreId()
+        {
+            var gig = new Gig();
+            var newVenue = "NewVenue";
+            var newDate = DateTime.Now;
+            var genreId = Byte.MinValue;
+
+            gig.Modify(newVenue, newDate, genreId);
+
+            gig.Venue.Should().Be(newVenue);
+            gig.DateTime.Should().Be(newDate);
+            gig.GenreId.Should().Be(genreId);
+        }
+
+        [Fact]
+        public void Modify_WhenCalled_EachAttendeeShouldHaveNotification()
+        {
+            var gig = new Gig();
+            gig.Attendances.Add(new Attendance { Attendee = new ApplicationUser { Id = "1" } });
+            var newVenue = "NewVenue";
+            var newDate = DateTime.Now;
+            var genreId = Byte.MinValue;
+
+            gig.Modify(newVenue, newDate, genreId);
+
+            var notifications = gig.GetAttendee().First().UserNotifications;
+
+            notifications.Count.Should().Be(1);
+            notifications.First().Notification.Type.Should().Be(NotificationType.GigUpdated);
+        }
+
         [Fact]
         public void Cancel_WhenCalled_ShouldSetIsCanceledToTrue()
         {
