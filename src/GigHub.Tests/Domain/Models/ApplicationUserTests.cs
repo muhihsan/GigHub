17d58cda6f5b20afce4a61bb15af4d6f424ceb76
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-25 14:30:06 +1000

    Add unit test for application user

---
+++ src/GigHub.Tests/Domain/Models/ApplicationUserTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using System.Linq;
+using Xunit;
+
+namespace GigHub.Tests.Domain.Models
+{
+    public class ApplicationUserTests
+    {
+        [Fact]
+        public void Notify_WhenCalled_ShouldAddTheNotification()
+        {
+            var user = new ApplicationUser();
+            var notification = Notification.GigCancelled(new Gig());
+
+            user.Notify(notification);
+
+            user.UserNotifications.Count.Should().Be(1);
+
+            var userNotification = user.UserNotifications.First();
+            userNotification.Notification.Should().Be(notification);
+            userNotification.User.Should().Be(user);
+        }
+    }
+}
