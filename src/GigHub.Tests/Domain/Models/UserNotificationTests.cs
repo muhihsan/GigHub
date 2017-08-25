b973824d92dbfefc8bd19f9fd0505881f511cff3
Muhammad Ihsan <mihsan191@gmail.com>
2017-08-25 16:20:41 +1000

    Add unit test for UserNotification

---
+++ src/GigHub.Tests/Domain/Models/UserNotificationTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using System;
+using Xunit;
+
+namespace GigHub.Tests.Domain.Models
+{
+    public class UserNotificationTests
+    {
+        [Fact]
+        public void Initialize_WhenNullUserPassed_ShouldThrowArgumentNullException()
+        {
+            Action act = () => new UserNotification(null, Notification.GigCreated(new Gig()));
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Initialize_WhenNullNotificationPassed_ShouldThrowArgumentNullException()
+        {
+            Action act = () => new UserNotification(new ApplicationUser(), null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Read_WhenCalled_ShouldSetIsReadToTrue()
+        {
+            var userNotification = new UserNotification(new ApplicationUser(), new Notification());
+
+            userNotification.Read();
+
+            userNotification.IsRead.Should().BeTrue();
+        }
+    }
+}
